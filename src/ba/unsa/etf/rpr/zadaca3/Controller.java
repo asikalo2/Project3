7c3b2c97ebf84e66912c4203ba6ba8760d300c1e
asikalo2 <asikalo2@etf.unsa.ba>
2019-01-11 14:05:45 +0100

    Dodani neki elementi u glavna.fxml.

--- src/ba/unsa/etf/rpr/zadaca3/Controller.java
+++ src/ba/unsa/etf/rpr/zadaca3/Controller.java
@@ -1,4 +1,11 @@
 package ba.unsa.etf.rpr.zadaca3;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void postaviSQLite(ActionEvent actionEvent) {
+    }
+
+    public void postaviXML(ActionEvent actionEvent) {
+    }
 }
